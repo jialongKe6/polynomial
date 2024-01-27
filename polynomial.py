75ae25a7bb030de89c23095923070be7c63845c9
jialongKe6 <jialongKe6>
2024-01-27 11:56:29 +0800

    cs 506 exercise part b

--- polynomial.py
+++ polynomial.py
@@ -20,20 +20,38 @@ class Add:
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
 
+
 class Mul:
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
     
     def __repr__(self):
-        if isinstance(self.p1, Add):
-            if isinstance(self.p2, Add):
-                 return "( " + repr(self.p1) + " ) * ( " + repr(self.p2) + " )"
-            return "( " + repr(self.p1) + " ) * " + repr(self.p2)
-        if isinstance(self.p2, Add):
-            return repr(self.p1) + " * ( " + repr(self.p2) + " )"
-        return repr(self.p1) + " * " + repr(self.p2)
+        def add_parentheses(expr):
+            return '(' + repr(expr) + ')' if isinstance(expr, (Add, Sub, Div)) else repr(expr)
 
+        p1_repr = add_parentheses(self.p1)
+        p2_repr = add_parentheses(self.p2)
+
+        return p1_repr + " * " + p2_repr
+class Sub:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+    
+    def __repr__(self):
+        return repr(self.p1) + " - " + repr(self.p2)
+
+class Div:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+    
+    def __repr__(self):
+        # Add parentheses around operands if they are instances of Add or Sub
+        p1_repr = '(' + repr(self.p1) + ')' if isinstance(self.p1, (Add, Sub)) else repr(self.p1)
+        p2_repr = '(' + repr(self.p2) + ')' if isinstance(self.p2, (Add, Sub)) else repr(self.p2)
+        return p1_repr + " / " + p2_repr
 
 poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
 print(poly)
