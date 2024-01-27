17e6512669f6ee7f5f3ab96af5aea34bad6e7613
jialongKe6 <jialongKe6>
2024-01-27 12:02:31 +0800

    cs 506 exercise part c

--- polynomial.py
+++ polynomial.py
@@ -5,12 +5,18 @@ class X:
     def __repr__(self):
         return "X"
 
+    def evaluate(self, x_value):
+        return x_value
+
 class Int:
     def __init__(self, i):
         self.i = i
     
     def __repr__(self):
         return str(self.i)
+
+    def evaluate(self, x_value):
+        return self.i
 
 class Add:
     def __init__(self, p1, p2):
@@ -20,12 +26,14 @@ class Add:
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
 
+    def evaluate(self, x_value):
+        return self.p1.evaluate(x_value) + self.p2.evaluate(x_value)
 
 class Mul:
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
-    
+
     def __repr__(self):
         def add_parentheses(expr):
             return '(' + repr(expr) + ')' if isinstance(expr, (Add, Sub, Div)) else repr(expr)
@@ -34,6 +42,10 @@ class Mul:
         p2_repr = add_parentheses(self.p2)
 
         return p1_repr + " * " + p2_repr
+
+    def evaluate(self, x_value):
+        return self.p1.evaluate(x_value) * self.p2.evaluate(x_value)
+
 class Sub:
     def __init__(self, p1, p2):
         self.p1 = p1
@@ -42,16 +54,22 @@ class Sub:
     def __repr__(self):
         return repr(self.p1) + " - " + repr(self.p2)
 
+    def evaluate(self, x_value):
+        return self.p1.evaluate(x_value) - self.p2.evaluate(x_value)
+
 class Div:
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
-    
+
     def __repr__(self):
-        # Add parentheses around operands if they are instances of Add or Sub
         p1_repr = '(' + repr(self.p1) + ')' if isinstance(self.p1, (Add, Sub)) else repr(self.p1)
         p2_repr = '(' + repr(self.p2) + ')' if isinstance(self.p2, (Add, Sub)) else repr(self.p2)
         return p1_repr + " / " + p2_repr
 
-poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
-print(poly)
+    def evaluate(self, x_value):
+        return self.p1.evaluate(x_value) / self.p2.evaluate(x_value)
+
+
+poly = Add(Add(Int(4), Int(3)), Add(X(), Mul(Int(1), Add(Mul(X(), X()), Int(1)))))
+print(poly.evaluate(-1)) 
